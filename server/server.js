109cf3ca93d92079f9f2568b6aa1655e38c1f64e
Sathvika-44 <tutika.sathvika@gmail.com>
2025-07-09 21:05:28 +0530

    cors updated

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  origin: process.env.CLIENT_URL,
   credentials: true, // allow cookies across origins
 }));
 app.use(express.json());
