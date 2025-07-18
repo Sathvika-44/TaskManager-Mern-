b916dababa3525b08b8429e58a6089326b09f78d
Sathvika-44 <tutika.sathvika@gmail.com>
2025-07-18 11:45:19 +0530

    updated login user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -29,7 +29,7 @@ export const loginUser = async (req, res) => {
     const user = await User.findOne({ email });
     if (!user) {
       console.error("❌ User not found:", email);
-      return res.status(400).json({ message: "Invalid email or password" });
+      return res.status(400).json({ message: "Invalid email" });
     }
 
     // ✅ Compare password securely
